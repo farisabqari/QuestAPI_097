48dd5c3e149aa3f5fd5cf4f2e598d96cdd2ea50d
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-30 14:29:21 +0800

    Menambahkan tampilan DetailScreen/view untuk menampilkan detail mahasiswa dengan fitur navigasi kembali dan tombol edit

--- app/src/main/java/com/example/myapplicationdatabase/ui/View/DetailView.kt
+++ app/src/main/java/com/example/myapplicationdatabase/ui/View/DetailView.kt
@@ -32,7 +32,7 @@ import com.example.myapplicationdatabase.ui.viewModel.PenyediaViewModel
 
 object DestinasiDetail: DestinasiNavigasi {
     override val route = "detail"
-    override val titleRes = "Detail Mhs"
+    override val titleRes = "Detail Mahsiswa"
     const val NIM = "nim"
     val routesWithArg = "$route/{$NIM}"
 }
@@ -77,77 +77,3 @@ fun DetailScreen(
     }
 }
 
-@Composable
-fun DetailStatus(
-    retryAction: () -> Unit,
-    modifier: Modifier = Modifier,
-    detailUiState: DetailUiState
-) {
-    when (detailUiState) {
-        is DetailUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
-
-        is DetailUiState.Success -> {
-            if (detailUiState.mahasiswa.nim.isEmpty()) {
-                Box(
-                    modifier = modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text("Data tidak ditemukan.")
-                }
-            } else {
-                ItemDetailMhs(
-                    mahasiswa = detailUiState.mahasiswa,
-                    modifier = modifier.fillMaxWidth()
-                )
-            }
-        }
-
-        is DetailUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
-    }
-}
-
-@Composable
-fun ItemDetailMhs(
-    modifier: Modifier = Modifier,
-    mahasiswa: Mahasiswa
-) {
-    Card(
-        modifier = modifier.padding(16.dp),
-        shape = MaterialTheme.shapes.medium,
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            ComponentDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
-            ComponentDetailMhs(judul = "Nama", isinya = mahasiswa.nama)
-            ComponentDetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
-            ComponentDetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
-            ComponentDetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
-            ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
-        }
-    }
-}
-
-@Composable
-fun ComponentDetailMhs(
-    modifier: Modifier =Modifier,
-    judul: String,
-    isinya: String,
-){
-    Column(modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.Start)
-    {
-        Text(
-            text = "$judul : ",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Gray
-        )
-        Text(
-            text = isinya,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
-        )
-    }
-}
