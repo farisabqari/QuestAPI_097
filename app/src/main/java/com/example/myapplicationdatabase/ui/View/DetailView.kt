bcd773a826c49530dfad63d34ecce62f0eba5e1a
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-30 14:30:55 +0800

    Menambahkan komponen ComponentDetailMhs untuk menampilkan detail mahasiswa dengan format judul dan isi

--- app/src/main/java/com/example/myapplicationdatabase/ui/View/DetailView.kt
+++ app/src/main/java/com/example/myapplicationdatabase/ui/View/DetailView.kt
@@ -129,3 +129,25 @@ fun ItemDetailMhs(
     }
 }
 
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier =Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start)
+    {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
