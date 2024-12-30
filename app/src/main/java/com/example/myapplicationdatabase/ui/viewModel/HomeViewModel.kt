a148eefd9dbcf365c7ff81c15ce260a2914b2faf
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-12-30 14:01:55 +0800

    Menambahkan fungsi getMhs untuk mengambil data Mahasiswa dengan penanganan error

--- app/src/main/java/com/example/myapplicationdatabase/ui/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplicationdatabase/ui/viewModel/HomeViewModel.kt
@@ -19,5 +19,20 @@ sealed class HomeUiState{
 class HomeViewModel (private val mhs: MahasiswaRepository): ViewModel(){
     var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
         private set
+    init {
+        getMhs()
+    }
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
 
 }
